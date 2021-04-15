9fb584f88cd252dd9a62cf07dc447c6d20841858
Mikael Brockman <mikael@brockman.se>
2021-04-15 09:55:11 +0300

    groan

--- proxy.js
+++ proxy.js
@@ -5,7 +5,7 @@ let proxy = require("cors-anywhere")
 proxy.createServer({
   originWhitelist: ["https://roamresearch.com"],
   handleInitialRequest: (req, res, url) => {
-    if (!url || !url.href.startsWith("https://api.telegram.com/file/bot")) {
+    if (!url || !url.href.startsWith("https://api.telegram.org/file/bot")) {
       res.writeHead(404)
       res.end("nope")
       return true
