293279c5707548b4593dc4e3692bf84c0c72d49f
Mikael Brockman <mikael@brockman.se>
2021-04-15 09:45:33 +0300

    banish evil

--- proxy.js
+++ proxy.js
@@ -3,7 +3,14 @@ let port = process.env.PORT || 8080
 
 let proxy = require("cors-anywhere")
 proxy.createServer({
-  originWhitelist: ["https://api.telegram.org"],
+  originWhitelist: ["https://roamresearch.com"],
+  handleInitialRequest: (req, res, url) => {
+    if (!url.startsWith("https://api.telegram.com/file/bot")) {
+      res.writeHead(404, "nope")
+      res.end()
+      return true
+    }
+  }
 }).listen(port, host, function () {
   console.log(`Telegram API CORS proxy running on ${host}:${port}`)
 })
