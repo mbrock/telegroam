b06fe106ce8353851e641ea70e2ae6188fa96d7b
Mikael Brockman <mikael@brockman.se>
2023-05-23 23:20:37 +0300

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -3,7 +3,7 @@ let port = process.env.PORT || 8080
 
 let proxy = require("cors-anywhere")
 proxy.createServer({
-  originWhitelist: ["https://roamresearch.com"],
+  originWhitelist: [],
   handleInitialRequest: (req, res, url) => {
     if (!url || !url.href.startsWith("https://api.telegram.org/file/bot")) {
       res.writeHead(404)
